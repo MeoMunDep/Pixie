7dbeffd1b4f8432d5d9af870d345575c1191d228
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-01 17:06:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,15 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "buyBoosts": true,
+  "buyCards": true,
+  "buySkins": false,
+  "buyCoins": true,
+  "countdown": 300,
+  "country_time": "vi-VN",
+  "limit": 1000,
+  "delayEachAccount": [1, 81],
+  "doTasks": true
 }
 EOL
 }
@@ -55,7 +54,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Pixie BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +114,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd pixi && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
